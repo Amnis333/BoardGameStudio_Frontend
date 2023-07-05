20b86f7bbe467e11d4cb364d9b5954f625cdb604
kohei <koheimtri3@gmail.com>
2023-07-05 22:27:34 +0900

    リクエストURLをlocalからboard-game-studio.netに修正

--- src/BoardController.ts
+++ src/BoardController.ts
@@ -16,7 +16,7 @@ export class ApiGateway {
     ];
     //todo リクエスト先をlocalhostから変更する
     const response = await axios.post(
-      "http://localhost:8000/start/",
+      "http://board-game-studio.net/start/",
       playerData,
       { withCredentials: true }
     );
@@ -32,7 +32,7 @@ export class ApiGateway {
   public static async notifyGetReady(tableInfo: Table): Promise<Table> {
     //全てのコマの初期位置が確定したらコマの位置情報をサーバーに送信する
     const response = await axios.post(
-      "http://localhost:8000/setup/",
+      "http://board-game-studio.net/setup/",
       tableInfo,
       { withCredentials: true }
     );
@@ -57,7 +57,7 @@ export class ApiGateway {
       destination: destination,
     };
     const response = await axios.post(
-      "http://localhost:8000/movement/",
+      "http://board-game-studio.net/movement/",
       movementInfo,
       { withCredentials: true }
     );
@@ -68,7 +68,7 @@ export class ApiGateway {
   }
   public static async cpuMovePiece(): Promise<Table> {
     const response = await axios.post(
-      "http://localhost:8000/cpu-movement/",
+      "http://board-game-studio.net/cpu-movement/",
       {},
       { withCredentials: true }
     );
